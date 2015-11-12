deaa10b55726fab48673bb418ceed1def7da2c78
Alexey Melnikov <alexey.ernest@gmail.com>
2015-11-13 05:27:20 +0600

    Menu fix

--- src/js/modules/signin.js
+++ src/js/modules/signin.js
@@ -28,28 +28,36 @@
       function ($scope, $state, authApi, user) {
 
         $scope.signin = {};
+        $scope.loading = false;
 
         $scope.submit = function (params) {
+          $scope.loading = true;
           authApi.signin(params.email, params.password, function (err, data) {
             if (err) { throw err; }
             user.init(data.accessToken, data.refreshToken);
+            $scope.loading = false;
+            
             $state.go('home');
           });
         };
 
         $scope.goFacebook = function () {
+          $scope.loading = true;
           authApi.facebook();
         };
 
         $scope.goTwitter = function () {
+          $scope.loading = true;
           authApi.twitter();
         };
 
         $scope.goGoogle = function () {
+          $scope.loading = true;
           authApi.google();
         };
 
         $scope.goVk = function () {
+          $scope.loading = true;
           authApi.vk();
         };
       }
--- src/js/modules/signup.js
+++ src/js/modules/signup.js
@@ -27,28 +27,35 @@
       function ($scope, $state, authApi) {
 
         $scope.signup = {};
+        $scope.loading = false;
 
         $scope.submit = function (params) {
+          $scope.loading = true;
           authApi.signup(params.email, params.password, function (err, data) {
             if (err) { return alert('Error: ' + err.message); }
-            alert('Ok: ' + JSON.stringify(data));
+            $scope.loading = false;
+
             $state.go('home');
           });
         };
 
         $scope.goFacebook = function () {
+          $scope.loading = true;
           authApi.facebook();
         };
 
         $scope.goTwitter = function () {
+          $scope.loading = true;
           authApi.twitter();
         };
 
         $scope.goGoogle = function () {
+          $scope.loading = true;
           authApi.google();
         };
 
         $scope.goVk = function () {
+          $scope.loading = true;
           authApi.vk();
         };
       }
