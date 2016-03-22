4f683782d6cf45159a0600ce445dd571885ad903
alexey-ernest <alexey.ernest@gmail.com>
2016-03-22 11:25:24 +0700

    WS-186 Show user name in Reshare post

--- dist/js/clbr.js
+++ dist/js/clbr.js
@@ -1472,35 +1472,47 @@
           attrs.shareUrl = $location.absUrl();
         }
 
-        attrs.shareTitle = (attrs.shareTitle || '').substring(0, 200);
-        attrs.shareDescription = (attrs.shareDescription || '').substring(0, 200);
-        attrs.shareImage = attrs.shareImage || '';
+        // listend for changes
+        var url, title, description, image;
+        attrs.$observe('shareUrl', function(shareUrl) {
+          url = encodeURIComponent(shareUrl);
+          updateShareUrl();
+        });
+        attrs.$observe('shareTitle', function(shareTitle) {
+          title = encodeURIComponent((shareTitle || '').substring(0, 200));
+          updateShareUrl();
+        });
+        attrs.$observe('shareDescription', function(shareDescription) {
+          description = encodeURIComponent((shareDescription || '').substring(0, 200));
+          updateShareUrl();
+        });
+        attrs.$observe('shareImage', function(shareImage) {
+          image = encodeURIComponent(shareImage || '');
+          updateShareUrl();
+        });
 
-        // encode
-        var url = encodeURIComponent(attrs.shareUrl);
-        var title = encodeURIComponent(attrs.shareTitle);
-        var description = encodeURIComponent(attrs.shareDescription);
-        var image = encodeURIComponent(attrs.shareImage);
-
-        // build share url
-        var shareUrl = null;
-        switch (attrs.shareType) {
-          case 'facebook': 
-            shareUrl = "https://www.facebook.com/sharer.php?u=" + url + "&t=" + title;
-            break;
-          case 'vk':
-            shareUrl = "http://vk.com/share.php?url=" + url + "&description=" + title + "&image=" + image;
-            break;
-          case 'google':
-            shareUrl = "https://plus.google.com/share?url=" + url;
-            break;
-          case 'twitter':
-            var twitterTitle = encodeURIComponent(decodeURIComponent(title).substring(0, 140));
-            shareUrl = "https://twitter.com/intent/tweet?url=" + url + "&text=" + twitterTitle;
-            break;
+        function updateShareUrl() {
+          var shareUrl = null;
+          switch (attrs.shareType) {
+            case 'facebook': 
+              shareUrl = "https://www.facebook.com/sharer.php?u=" + url + "&t=" + title;
+              break;
+            case 'vk':
+              shareUrl = "http://vk.com/share.php?url=" + url + "&description=" + title + "&image=" + image;
+              break;
+            case 'google':
+              shareUrl = "https://plus.google.com/share?url=" + url;
+              break;
+            case 'twitter':
+              var twitterTitle = encodeURIComponent(decodeURIComponent(title).substring(0, 140));
+              shareUrl = "https://twitter.com/intent/tweet?url=" + url + "&text=" + twitterTitle;
+              break;
+          }
+          element.attr('href', shareUrl);
         }
 
-        element.attr('href', shareUrl);
+        // set share url
+        updateShareUrl();
         element.attr('target', "_blank");
       }
     };
